76719c193a4bf97864972b2256d99b2cbe8d6922
SYS_0xA7 <ktme156@gmail.com>
2025-12-30 23:15:02 +0300

    Update .ps1

--- .ps1
+++ .ps1
@@ -1,1 +1,1 @@
-irm https://raw.githubusercontent.com/WolfGames156/Zoream-Database/refs/heads/main/fix-st.ps1 | iex
+irm https://raw.githubusercontent.com/WolfGames156/Zoream-Database/main/fix-st.ps1 | iex
